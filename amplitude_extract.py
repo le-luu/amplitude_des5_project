f1ef5b0ba423669af77646877d2a3580d464dfc7
Le Luu <le.luu.dev@gmail.com>
2026-01-08 11:35:36 +0000

    fixing errors

--- amplitude_extract.py
+++ amplitude_extract.py
@@ -17,8 +17,7 @@ params = {
 #Load API Keys
 load_dotenv()
 api_key=os.getenv('AMP_API_KEY')
-secret_key=os.getenv('AMP_SECRET_KEY')
-
+secret_key=os.getenv('AMP_API_SECRET')
 
 # Set up Logging
 logs_dir = "logs"
@@ -34,13 +33,13 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 # Send request and get response
-number_of_retries = 5
+number_of_retries = 20
 count = 0
 
 while count< number_of_retries:
     try:
         logger.info(f"Sending request to Amplitude API: {url} with params: {params}")
-        response = requests.get(url, params=params, auth=(api_key, secret_key),timeout=10)
+        response = requests.get(url, params=params, auth=(api_key, secret_key))
     except requests.exceptions.RequestException as e:
         print(f"Request failed: {e}")
         logger.error(f"Request failed: {e}")
@@ -62,7 +61,7 @@ while count< number_of_retries:
 
     response_code = response.status_code
 
-    if response_code != 200:
+    if response_code == 200:
 
         dir = "data"
         if not os.path.exists(dir):
@@ -72,11 +71,7 @@ while count< number_of_retries:
         filepath = f"{dir}/data_{filename}.zip"
         print('Data retrieved successfully.')
         logger.info('Data retrieved successfully.')
-
-        # JSON data files saved to a zip folder 'data.zip'
-        with open(filepath, 'wb') as file:
-            file.write(data)
-
+        
         try:
             with open(filepath, 'wb') as file:
                 file.write(data)
@@ -89,13 +84,13 @@ while count< number_of_retries:
         break
 
     elif response_code > 499 or response_code < 200:
-        print(response.reason)
+        print(response_code,response.reason)
         logger.warning(response.reason)
         time.sleep(10)
         count += 1
         continue
     else:
-        print(response.reason)
+        print(response_code, response.reason)
         logger.error(response.reason)
         break
 
